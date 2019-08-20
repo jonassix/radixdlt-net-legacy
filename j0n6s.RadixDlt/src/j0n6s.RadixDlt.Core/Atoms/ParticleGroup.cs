f4539b5da6edd00dc383f9996b1c51930ad0d46b
jonas452 <jonas.six452@gmail.com>
2019-08-20 08:35:33 +0200

    finished particlegroup(the basis)

--- j0n6s.RadixDlt/src/j0n6s.RadixDlt.Core/Atoms/ParticleGroup.cs
+++ j0n6s.RadixDlt/src/j0n6s.RadixDlt.Core/Atoms/ParticleGroup.cs
@@ -2,12 +2,26 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Text;
 
 namespace j0n6s.RadixDlt.Atoms
 {
-    public class ParticleGroup
+    public class ParticleGroup<T>
+        where T : Particle
     {
-        public ImmutableList<SpunParticle> MyProperty { get; set; }
+        public ImmutableList<SpunParticle<T>> Particles { get; }
+        public ImmutableDictionary<string,string> MetaData { get; }
+
+        public ParticleGroup(ImmutableList<SpunParticle<T>> particles, ImmutableDictionary<string, string> metaData)
+        {
+            Particles = particles;
+            MetaData = metaData;
+        }
+
+        public IEnumerable<Particle> GetParticlesWithSpin(Spin spin)
+        {
+            return Particles.Where(p => p.Spin == spin).Select(p => p.Particle);
+        }
     }
 }
